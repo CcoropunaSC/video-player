5ea311491f17379805c977f59e59885d04755d1d
CcoropunaSC <celso.ccoropuna.s@gmail.com>
2023-02-02 15:28:13 -0500

    Extra

--- js/player.js
+++ js/player.js
@@ -4,6 +4,10 @@ const $pause = document.querySelector("#pause");
 const $backward = document.querySelector("#backward");
 const $forward = document.querySelector("#forward");
 const $progress = document.querySelector("#progress");
+const $currentTime = document.querySelector("#currentTime");
+const $endTime = document.querySelector("#endTime");
+const $speakerOn = document.querySelector("#speakerOn");
+const $speakerOff = document.querySelector("#speakerOff");
 
 const init = () => {
     $pause.hidden = true;
@@ -39,23 +43,44 @@ $backward.addEventListener("click", handleBackward);
 $forward.addEventListener("click", handleForward);
 
 const handleTimeUpdate = () => {
-    $progress.value = $video.currentTime
-    // console.log($video.currentTime);
+    const videoCurrentTime = $video.currentTime;
+    const currentMinutes = Math.floor(videoCurrentTime / 60);
+    const remainderSeconds = Math.floor(videoCurrentTime % 60);
+
+    $progress.value = videoCurrentTime;
+    $currentTime.innerHTML = `${currentMinutes}:${remainderSeconds}`;
 }
 
 const handleLoaded = () => {
-    $progress.max = $video.duration;
-    console.log($progress);
-    console.log('video loaded:' + $video.duration);
+    const videoDuration = $video.duration;
+    const durationMinutes = Math.floor(videoDuration / 60);
+    const remainderSeconds = Math.floor(videoDuration % 60);
+
+    $progress.max = videoDuration;
+    $endTime.innerHTML = `${durationMinutes}:${remainderSeconds}`;
 }
 
 const handleInput = () => {
     $video.currentTime = $progress.value;
 }
 
+const handleToggleMuted = () => {
+    if($video.muted) {
+        $video.muted = false;
+        $speakerOn.hidden = false;
+        $speakerOff.hidden = true;
+    } else {
+        $video.muted = true;
+        $speakerOn.hidden = true;
+        $speakerOff.hidden = false;
+    }
+}
+
 $video.addEventListener('loadedmetadata', handleLoaded)
 $video.addEventListener('timeupdate', handleTimeUpdate)
 $progress.addEventListener('input', handleInput)
+$speakerOn.addEventListener('click', handleToggleMuted)
+$speakerOff.addEventListener('click', handleToggleMuted)
 
 
 
